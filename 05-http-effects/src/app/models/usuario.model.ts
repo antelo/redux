e77079abec4309fac91d94fd28bdee33c952c7a6
antelo <antelo03@msn.com>
2019-08-18 13:56:41 +0200

    Seccion 12 lista - Effects

---
+++ 05-http-effects/src/app/models/usuario.model.ts
@@ -0,0 +1,11 @@
+
+
+
+export class Usuario {
+    constructor(
+        public id: string,
+        public fist_name: string,
+        public last_name: string,
+        public avatar: string
+    ) {}
+}
